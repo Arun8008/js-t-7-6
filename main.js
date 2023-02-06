f8d981c745c7cafbbb43c4b8493ba467b29d20b8
Arun8008 <arun.sivanpandian.2000@gmail.com>
2023-02-06 16:18:43 +0530

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+/*----------6--------*/
+
+const numbers = [1, 2, 3, 4, 5];
+			const mul = numbers.reduce(function (v1, v2) {
+			arun= v1 * v2;
+				  return arun;
+				});
+			console.log(mul/3);
+			document.write("<br>",mul/3);
